24b30c7c514f31b3540a7fa9e12a56a16005d10e
kjminn <kjminn2001@gmail.com>
2020-11-19 19:36:23 +0900

    Complete ImageDialog

--- app/src/main/java/kr/ac/ssu/kjminn/mirimdialogtest3/MainActivity.java
+++ app/src/main/java/kr/ac/ssu/kjminn/mirimdialogtest3/MainActivity.java
@@ -1,5 +1,6 @@
 package kr.ac.ssu.kjminn.mirimdialogtest3;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -20,6 +21,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         rg = findViewById(R.id.rg);
+        radioPark = findViewById(R.id.radio_park);
+        radioPark2 = findViewById(R.id.radio_hana);
+        radioSon = findViewById(R.id.radio_son);
+        radioAhn = findViewById(R.id.radio_ahn);
         Button btnShow = findViewById(R.id.btn_show);
+        btnShow.setOnClickListener(btnShowListener);
     }
+    
+    View.OnClickListener btnShowListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            dialogView = View.inflate(MainActivity.this, R.layout.dialog, null);
+            dlgImg = dialogView.findViewById(R.id.img);
+            int selectedId = 0;
+            String seletedTitle = "";
+            switch (rg.getCheckedRadioButtonId()){
+                case R.id.radio_park:
+                    selectedId = R.drawable.seripark;
+                    seletedTitle = radioPark.getText().toString();
+                    break;
+                case R.id.radio_hana:
+                    selectedId = R.drawable.park2;
+                    seletedTitle = radioPark2.getText().toString();
+                    break;
+                case R.id.radio_son:
+                    selectedId = R.drawable.son;
+                    seletedTitle = radioSon.getText().toString();
+                    break;
+                case R.id.radio_ahn:
+                    selectedId = R.drawable.ahn;
+                    seletedTitle = radioAhn.getText().toString();
+                    break;
+            }
+            AlertDialog. Builder  dlg = new AlertDialog.Builder(MainActivity.this);
+            dlg.setTitle(seletedTitle);
+            dlgImg.setImageResource(selectedId);
+            dlg.setView(dialogView);
+            dlg.setPositiveButton("닫기", null);
+            dlg.show();
+        }
+    };
 }
